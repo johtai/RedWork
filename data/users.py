bc98adb9e2e64cbcc0c412265a221a43be472df8
johtai <peterwaybright@gmail.com>
2025-03-04 21:59:52 +0300

    profile was added

--- data/users.py
+++ data/users.py
@@ -4,6 +4,7 @@ from sqlalchemy import orm
 from werkzeug.security import generate_password_hash, check_password_hash
 from .db_session import SqlAlchemyBase
 from flask_login import UserMixin
+from hashlib import md5
 
 
 class User(SqlAlchemyBase, UserMixin):
@@ -16,14 +17,16 @@ class User(SqlAlchemyBase, UserMixin):
     email = sqlalchemy.Column(sqlalchemy.String, 
                               index=True, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    created_date = sqlalchemy.Column(sqlalchemy.DateTime, 
-                                     default=datetime.datetime.now)
+    created_date = sqlalchemy.Column(sqlalchemy.String,
+                                     default=datetime.datetime.today().strftime("%d.%m.%Y %H:%M"))
     jobs = orm.relation("Jobs", back_populates='user')
-
 
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
 
-
     def check_password(self, password):
         return check_password_hash(self.hashed_password, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
